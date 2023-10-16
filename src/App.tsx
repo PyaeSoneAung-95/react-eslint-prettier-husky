77737600dab90afe72b8cdd5d08385332d7ecfac
PyaeSoneAung <pyaesoneaungwebdp@gmail.com>
2023-10-16 15:24:48 +0630

    make  some changes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 function App() {
+
+  
   return (
     <>
       <button disabled type="button">
         Click me
       </button>
+      <p>Make some changes in App component!</p>
     </>
   );
 }
