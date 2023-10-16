8064c5aead1be125f1315672fa94aa3d52f01a9a
PyaeSoneAung <pyaesoneaungwebdp@gmail.com>
2023-10-16 20:29:30 +0630

    add pre push

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ describe("App", () => {
   test("button is disabled", () => {
     render(<App />);
 
-    const button = screen.getByRole("button");
+    const button = screen.getByRole("button", { name: "Click me" });
     expect(button).toBeEnabled();
   });
 });
