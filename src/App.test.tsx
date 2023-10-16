c5a8064c7dbc19bfca7cf5e310c7d4a8a4c38892
PyaeSoneAung <pyaesoneaungwebdp@gmail.com>
2023-10-16 19:10:38 +0630

    test app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,9 +5,6 @@ describe("App", () => {
   test("button is disabled", () => {
     render(<App />);
     const button = screen.getByRole("button");
-    const name = "psa";
-    const fullname = `Pyae ${name}`;
-    console.log(fullname);
-    expect(button).toBeDisabled();
+    expect(button).toBeEnabled();
   });
 });
