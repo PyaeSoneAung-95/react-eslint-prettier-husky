bd8c124ee88dad4ecdca494d23c3ad4a707e0a50
PyaeSoneAung <pyaesoneaungwebdp@gmail.com>
2023-10-16 16:29:57 +0630

    let make some changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <p>Make some changes in App component!</p>
       <p>Add another text</p>
       <button>Add some button</button>
+      <button>Hi i am a button with long text </button>
     </>
   );
 }
