901897cab5d0a260fdef4e0da32a2c9b5bf085ec
PyaeSoneAung <pyaesoneaungwebdp@gmail.com>
2023-10-16 16:23:56 +0630

    formatted code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 function App() {
   return (
-
     <>
       <button disabled type="button">
         Click me
       </button>
       <p>Make some changes in App component!</p>
       <p>Add another text</p>
-      <button>Add some button
-
-        
-      </button>
+      <button>Add some button</button>
     </>
   );
 }
