f41addd5966451d82a13e18d1d800158c68d41cc
PyaeSoneAung <pyaesoneaungwebdp@gmail.com>
2023-10-16 15:29:15 +0630

    add another text

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 function App() {
 
-  
+
   return (
     <>
       <button disabled type="button">
         Click me
       </button>
       <p>Make some changes in App component!</p>
+      <p>Add another text</p>
     </>
+
+    
   );
 }
 
