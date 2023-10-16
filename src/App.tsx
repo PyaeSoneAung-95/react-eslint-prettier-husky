f1656647854cfaf343eefbd1f26cd3a8a02076a9
PyaeSoneAung <pyaesoneaungwebdp@gmail.com>
2023-10-16 21:34:29 +0630

    let button enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
   return (
     <>
-      <button disabled type="button">
-        Click me
-      </button>
+      <button type="button">Click me</button>
       <p>Make some changes in App component!</p>
       <p>Add another text</p>
       <button>Add some button</button>
